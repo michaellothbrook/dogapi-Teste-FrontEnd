d4b9416554c10e3631a05e3d3a49c8f377ce46b6
Michael Duarte <michael.lothbrook@gmail.com>
2020-02-03 21:47:28 -0300

    Final

--- js/api.js
+++ js/api.js
@@ -62,6 +62,65 @@
             console.log(font);
             $('head').append("<link href='https://fonts.googleapis.com/css?family=" + font + "' rel='stylesheet' type='text/css'>");
             $('body').css('font-family',font[0]);
-          })
+          });
+
+          //SAVE LOCALSTORAGE
+          function Adicionar(){
+            var data = localStorage.getItem("Dados API");// Recupera os dados armazenados
+            data = JSON.parse(data); // Converte string para objeto
+
+            var dados_form = JSON.stringify({
+                Raça : $("#racas").val(),
+                Nome : $("#nomeCachorro").val(),
+                Cor  : $("#cor").val(),
+                Fonte :$('#fonte').val() 
+    
+            });
+            console.log(dados_form);
+            if(data == null) {
+              data = Array();
+            }
+            data.push(dados_form);
+            localStorage.setItem("Dados API", JSON.stringify(data));
+            return true;
+            console.log(data);
+          }
+        
+          $('#formCadastro').on('submit', function(e) {
+            e.preventDefault();
+            $('#btn-submit').attr('disabled',true);
+            $('#btn-submit').html(`
+              <div class="spinner-border" role="status">
+                <span class="sr-only">Carregando</span>
+              </div>Aguarde...
+            `);
+            $.get(window.location.href)
+            .done(function(data){
+              if(Adicionar()) {
+                $('#sucesso').remove();
+                $('#btn-message').fadeIn().append(`
+                <div id="sucesso" class="alert alert-success mt-3" role="alert">
+                  Cadastro realizado com sucesso!
+                </div>
+                `);
+                $('#formCadastro').each (function(){
+                  this.reset();
+                });
+              }
+              $('#btn-submit').attr('disabled',false);
+              $('#btn-submit').text('Cadastrar');
+            })
+            .fail(function(){
+              $('#erro').remove();
+              $('#btn-message').fadeIn().append(`
+                <div id="erro" class="alert alert-danger mt-3" role="alert">
+                  Não foi possível realizar o cadastro, Tente novamente mais tarde!
+                </div>
+                `);
+                $('#formCadastro').each (function(){
+                  this.reset();
+                });
+            })
+          });
 
         });
